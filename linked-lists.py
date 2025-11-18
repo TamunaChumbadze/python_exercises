bb26c750b5a98711a32ce2edebb12da905286d52
TamunaChumbadze <t.chumbadze@gmail.com>
2025-11-18 20:41:57 +0400

    Add reverse() method test to linked list

--- linked-lists.py
+++ linked-lists.py
@@ -84,18 +84,64 @@ class LinkedLists:
                         temp.value = value
                         return True
                 return False
+        def insert(self, index, value):
+                if index < 0 or index > self.length:
+                        return False
+                if index == 0:
+                        self.prepend(value)
+                        return True
+                if index == self.length:
+                        self.append(value)
+                        return True
+                new_node = Node(value)
+                temp = self.get(index - 1)
+                new_node.next = temp.next
+                temp.next = new_node
+                self.length += 1
+                return True
         
+        def remove(self, index):
+                if index < 0 or index >= self.length:
+                        return None
+                if index == 0:
+                        return self.pop_first()
+                if index == self.length - 1:
+                        return self.pop()
+                pre = self.get(index - 1)
+                temp = pre.next
+                pre.next = temp.next
+                temp.next = None
+                self.length -= 1
+                return temp
+        
+        def reverse(self):
+                temp = self.head
+                self.head = self.tail
+                self.tail = temp
+                after = temp.next
+                before = None
+                for _ in range(self.length):
+                        after = temp.next
+                        temp.next = before
+                        before = temp
+                        temp = after
 
-my_linked_list = LinkedLists(10)
-my_linked_list.append(20)
-my_linked_list.append(30)
+my_linked_list = LinkedLists(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
 
-print("Before:")
+print("Befor reverse:")
 my_linked_list.print_list()
 
-my_linked_list.set_value(1, 200)
-print("\nAfter:")
+my_linked_list.reverse()
+
+print("\nAfter reverse:")
 my_linked_list.print_list()
+
+
+
+
 
 
 
@@ -164,3 +210,39 @@ my_linked_list.print_list()
 # my_linked_list.append(3)
 # result = my_linked_list.get(2).value
 # print(result)
+
+# my_linked_list = LinkedLists(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+
+# print("Before:")
+# my_linked_list.print_list()
+
+# my_linked_list.set_value(1, 4)
+# print("\nAfter:")
+# my_linked_list.print_list()
+
+
+# my_linked_list = LinkedLists(2)
+# my_linked_list.prepend(0)
+
+# print("Before insert():")
+# my_linked_list.print_list()
+
+# my_linked_list.insert(1, 1)
+# print("\nAfter insert():")
+# my_linked_list.print_list()
+
+
+# my_linked_list = LinkedLists(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+
+# print("Before remove():")
+# my_linked_list.print_list()
+
+# my_linked_list.remove(2)
+# print("\nAfter remove():")
+# my_linked_list.print_list()
