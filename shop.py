83a0f41e3fbbbf8c9c1801a9a736a2a379a96acc
TamunaChumbadze <t.chumbadze@gmail.com>
2025-11-25 21:03:44 +0400

    added display_product

---
+++ shop.py
@@ -0,0 +1,12 @@
+inventory = [
+    {
+        "name": "Vintage T-shirt",
+        "price": 29.99,
+        "stock": 41
+    }
+]
+
+def display_products(inventory):
+    print(inventory[0]["name"])
+    
+display_products(inventory)
