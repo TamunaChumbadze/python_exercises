e952b6e2cca25ebd147831280cc96154c18cd2ce
TamunaChumbadze <t.chumbadze@gmail.com>
2025-11-25 21:29:25 +0400

    modified dysplay_prodacts

--- shop.py
+++ shop.py
@@ -2,11 +2,27 @@ inventory = [
     {
         "name": "Vintage T-shirt",
         "price": 29.99,
-        "stock": 41
+        "stock": 41,
+    },
+    {
+        "name": "Coffe Mug",
+        "price": 12.50,
+        "stock": 100,
+    },
+    {
+        "name": "Laptop Sticker Pack",
+        "price": 5.00,
+        "stock": 250,
     }
+
 ]
 
 def display_products(inventory):
-    print(inventory[0]["name"])
-    
+    for index, item in enumerate(inventory):
+        print(
+            f"{index + 1}. Name: {item['name']} - Price: ${item['price']} - Stock: {item['stock']}"
+        )
+   
+
+
 display_products(inventory)
